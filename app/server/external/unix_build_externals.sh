8d8e1fa4b4650947230f9888815f7661ae1d9476
SunderB <20426079+SunderB@users.noreply.github.com>
2020-02-26 17:36:31 +0000

    Update unix_build_externals.sh

--- app/server/external/unix_build_externals.sh
+++ app/server/external/unix_build_externals.sh
@@ -6,7 +6,12 @@ echo "Building server external dependencies..."
 mkdir -p "${SCRIPT_DIR}/build"
 cd "${SCRIPT_DIR}/build"
 cmake -G "Unix Makefiles" ..
-cmake --build .
+
+cmake --build . --target osmid
+
+if [ $1 = "--build-aubio" ]; then
+  cmake --build . --target aubio
+fi
 
 #dont remove ruby-aubio-prerelease  as needed in linux build
 #it is removed in the windows-prebuild
